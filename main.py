9ff1b9dd624e9cb80d605e74603eba8058751793
Johnny Matos <johnny.datascientist@gmail.com>
2024-05-21 01:00:40 -0300

    Instalado biblioteca pwinput e emplementado código para não exibir o input de senha no terminal

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import subprocess
+import pwinput
 
 
+password = "johnny*03"
 
 
-if __name__ == "__main__":
-    subprocess.run(["streamlit", "run", "Home.py"])
+request = pwinput.pwinput(prompt='Insira a senha para acessar o sistema: ')
+
+
+if request == password:
+    print("Senha correta!")
+else:
+    print("Senha incorreta!")
+
+
+#if __name__ == "__main__":
+#    subprocess.run(["streamlit", "run", "Home.py"])
