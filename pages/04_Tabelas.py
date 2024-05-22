1690864b0ba9db3b5acd185777814c09032b5b2a
Johnny Matos <johnny.datascientist@gmail.com>
2024-05-22 04:53:49 -0300

    Criado filtros para a sessão da tabela de movimentações

--- pages/04_Tabelas.py
+++ pages/04_Tabelas.py
@@ -14,7 +14,36 @@ df = pd.DataFrame(dados) # Cria Dataframe com os dados da query
 moviment, contas = st.tabs(['Movimentações', 'Contas']) # Divide em duas guias
 
 with moviment:
-    df = df[['data', 'tipo', 'categoria', 'conta', 'comentario', 'valor']] # Seleciona as colunas do DataFrame a serem exibidas
+    col1, col2 = st.columns(2)
+    with col1:
+        tipo = st.multiselect(
+        "Tipo:", ['Entrada','Saída'],
+        default=['Entrada', 'Saída']
+        )
+        
+        start_date, end_date = st.select_slider(
+        "Selecione o período:",
+        options=pd.Series(df['data'].unique()).sort_values(),
+        value=(df['data'].min(), df['data'].max())
+        )
+    with col2:
+
+        conta = st.multiselect(
+            "Conta:",
+            df['conta'].unique().tolist(),
+            default=df['conta'].unique().tolist()
+        )
+        
+        categoria = st.multiselect(
+            "Categoria:",
+            df['categoria'].unique().tolist(),
+            default=df['categoria'].unique().tolist()
+        )
+        
+    
+    df_filtrado = df[(df['conta'].isin(conta)) & (df['tipo'].isin(tipo)) & (df['categoria'].isin(categoria)) & (df['data'] >= start_date) & (df['data'] <= end_date)]
+    
+    df = df_filtrado[['data', 'tipo', 'categoria', 'conta', 'comentario', 'valor']] # Seleciona as colunas do DataFrame a serem exibidas
     st.dataframe(df, use_container_width=True) # Exibe dataframe
 
 with contas:
