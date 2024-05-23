ec439b3b4df05bb51083f9f2b7b092fb4315a532
Johnny Matos <johnny.datascientist@gmail.com>
2024-05-23 12:45:59 -0300

    Finalizado barras de progresso na página de dashboard

--- .ipynb_checkpoints/Home-checkpoint.py
+++ .ipynb_checkpoints/Home-checkpoint.py
@@ -3,6 +3,11 @@ import streamlit as st
 st.title("Fluxo de caixa")
 
 
+    
+else:
+    st.warning("Não existem registros de transações ainda.")
+
+
 bars = [{
     "categoria" : "Lazer",
     "limite" : 200
@@ -13,11 +18,18 @@ bars = [{
 }
 ]
 
-gasto = 75
+gasto = 270
 
 
 for i in range(len(bars)):
     categoria = "Limite " + bars[i]['categoria']
     orcament = gasto/bars[i]['limite']
-    st.sidebar.progress(orcament, text=categoria)
     
+    if orcament > 1.0:
+        my_bar = st.sidebar.progress(1.0, text=categoria)
+    else:
+        my_bar = st.sidebar.progress(orcament, text=categoria)
+    
+# BARRAS DE PROGRESSO
+
+#CONSULTA 
--- pages/02_Dashboard.py
+++ pages/02_Dashboard.py
@@ -1,27 +1,36 @@
 import streamlit as st
+import db_manager as dbm
+import pandas as pd
 
+transacoes = dbm.consulta_base()
 
-
-
-bars = [{
-    "categoria" : "Lazer",
-    "limite" : 200
-},
-{
-    "categoria" : "Alimentação",
-    "limite" : 300
-}
-]
-
-gasto = 270
-
-
-for i in range(len(bars)):
-    categoria = "Limite " + bars[i]['categoria']
-    orcament = gasto/bars[i]['limite']
+if transacoes:
+    df = pd.DataFrame(transacoes)
+    st.dataframe(df[['data','tipo','categoria','conta','valor','descricao']], use_container_width=True)
     
-    if orcament > 1.0:
-        my_bar = st.sidebar.progress(1.0, text=categoria)
+    # Barras de Progresso
+    st.sidebar.title("Orçamento:")
+    
+    # Identificar orcamentos
+    orcamentos = dbm.consultar_orcamentos() # QUERY tabela orçamentos
+    # Se houver registros na tabela de orçamento faz o cálculo e exibe a barra de progresso
+    if orcamentos:
+        # Fazer o cálculo do progresso para cada despesa
+        for i in range(len(orcamentos)):
+            categoria = orcamentos[i]['categoria']
+            despesa = df[df['categoria'] == categoria]["valor"].sum() # Soma o gasto total por categoria
+            limite = orcamentos[i]['limite']
+            orcamento = despesa/limite # Calcula a porcentagem do progresso
+
+            #Exibir barra de progresso
+            if orcamento > 1.0:
+                st.sidebar.progress(1.0, text=categoria) # Se o gasto ultrapassar o orçamento, exibe a barra completa para evitar erro do widget
+            else:
+                st.sidebar.progress(orcamento, text=categoria) # Exibe o progresso de acordo com a porcentagem
     else:
-        my_bar = st.sidebar.progress(orcament, text=categoria)
-    
+        st.sidebar.warning("Não foram encontrados orçamentos para despesas.")
+
+else:
+    st.warning("Não existem registros de transações ainda.")
+
+
