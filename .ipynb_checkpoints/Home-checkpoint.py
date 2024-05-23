e60bfadad62903c5c599eaf9b1baea244eb0363e
Johnny Matos <johnny.datascientist@gmail.com>
2024-05-23 15:18:31 -0300

    Aplicado filtro de mês na página Dashboard

--- .ipynb_checkpoints/Home-checkpoint.py
+++ .ipynb_checkpoints/Home-checkpoint.py
@@ -4,32 +4,3 @@ st.title("Fluxo de caixa")
 
 
     
-else:
-    st.warning("Não existem registros de transações ainda.")
-
-
-bars = [{
-    "categoria" : "Lazer",
-    "limite" : 200
-},
-{
-    "categoria" : "Alimentação",
-    "limite" : 300
-}
-]
-
-gasto = 270
-
-
-for i in range(len(bars)):
-    categoria = "Limite " + bars[i]['categoria']
-    orcament = gasto/bars[i]['limite']
-    
-    if orcament > 1.0:
-        my_bar = st.sidebar.progress(1.0, text=categoria)
-    else:
-        my_bar = st.sidebar.progress(orcament, text=categoria)
-    
-# BARRAS DE PROGRESSO
-
-#CONSULTA 
--- pages/02_Dashboard.py
+++ pages/02_Dashboard.py
@@ -4,11 +4,64 @@ import pandas as pd
 
 transacoes = dbm.consulta_base()
 
+def mes(data : str):
+    meses = {
+        "01" : "Janeiro",
+        "02" : "Fevereiro",
+        "03" : "Março",
+        "04" : "Abril",
+        "05" : "Maio",
+        "06" : "Junho",
+        "07" : "Julho",
+        "08" : "Agosto",
+        "09" : "Setembro",
+        "10" : "Outubro",
+        "11" : "Novembro",
+        "12" : "Dezembro"}
+    
+    return meses[data]
+
 if transacoes:
     df = pd.DataFrame(transacoes)
-    st.dataframe(df[['data','tipo','categoria','conta','valor','descricao']], use_container_width=True)
+    df['mes'] = df['data'].apply(lambda x: mes(x[5:7])) # Cria uma coluna com o nome dos meses
     
-    # Barras de Progresso
+    mes = st.sidebar.selectbox(
+    "Selecione o mês:",
+    df['mes'].unique(),
+    key='mes'
+    )
+    
+    df_mes = df[df['mes'] == mes]
+    df = df_mes
+    # =-=--=-=-=-=--=-=-=-=-=-=-=-=-=- CORPO DA PÁGINA
+    st.title("Dashboard")
+    st.markdown('''---''')
+    
+    #ENTRADA
+    with st.container():
+        st.markdown("### Entrada")
+        ent_filtros, ent_graficos = st.columns(2)
+        with ent_filtros:
+            st.write("Filtros")
+        with ent_graficos:
+            st.write("Gráficos")
+    
+    #SAIDA
+    with st.container():
+        st.markdown("### Saída")
+        sai_filtros, sai_graficos = st.columns(2)
+        with sai_filtros:
+            st.write("Filtros")
+        with sai_graficos:
+            st.write("Gráficos")
+    
+    
+    
+    
+    
+    
+    
+    # =-=--=-=-=-=--=-=-=-=-=-=-=-=-=- Barras de Progresso
     st.sidebar.title("Orçamento:")
     
     # Identificar orcamentos
@@ -29,6 +82,10 @@ if transacoes:
                 st.sidebar.progress(orcamento, text=categoria) # Exibe o progresso de acordo com a porcentagem
     else:
         st.sidebar.warning("Não foram encontrados orçamentos para despesas.")
+    
+    
+    #EXIBE  A TABELA DE TRANSAÇÕES NO FINAL
+    st.dataframe(df[['data','tipo','categoria','conta','valor','descricao']], use_container_width=True)
 
 else:
     st.warning("Não existem registros de transações ainda.")
