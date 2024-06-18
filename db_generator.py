0e88249f79d3c9421da1d154ad6624a10a9dd069
Johnny Matos <128853698+DS-Johnny@users.noreply.github.com>
2024-06-18 08:18:06 -0300

    db_generator.py refatorado e funcionando

--- db_generator.py
+++ db_generator.py
@@ -1,37 +1,46 @@
 import sqlite3
 from sqlite3 import Error
 
-
-#CRIA A CONEXÃO COM O BANCO DE DADOS
-def create_connection(db_file):
-    """ Cria conexão com o banco de dados SQLite
-        especificado por arquivo_db
-        :param arquivo_db: diretório do banco de dados
-        :return : Objeto de conexão ou None
+class ConexaoDb:
     """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        return e
-
-    return conn
-
-
-# CRIA UMA FUNÇÃO GERADORA DE TABELAS
-def create_table(conn, create_table_sql):
-
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        return e
+    Classe para gerenciar a conexão e criação de tabelas em um banco de dados SQLite.
+    """
+    def __init__(self, database) -> None:
+        self.database = database
     
+    def generate_conn(self):
+        """
+        Cria uma conexão com o banco de dados SQLite.
+
+        :return: Objeto de conexão ou None em caso de erro.
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect(self.database)
+            return conn
+        except Error as e:
+            print(f"Erro ao conectar ao banco de dados: {e}")
+
+    def create_table(self, conn, sql):
+        """
+        Cria uma tabela no banco de dados.
+
+        :param conn: Objeto de conexão com o banco de dados.
+        :param sql: Comando SQL para criar a tabela.
+        """
+        try:
+            cur = conn.cursor()
+            cur.execute(sql)
+        except Error as e:
+            print(e)
 
 def main():
-    database = "fluxo.db"
-
+    """
+    Função principal que define os comandos SQL para criar tabelas e executa a criação.
+    """
+    # Inicializa a conexão com o banco de dados
+    db = ConexaoDb('database.db')
+    # Comandos SQL para criar as tabelas
     sql_create_transacoes_table = """CREATE TABLE IF NOT EXISTS transacoes(
                                     id_transacao INTEGER primary key autoincrement,
                                     data DATE not null,
@@ -53,55 +62,33 @@ def main():
                                 id_categoria INTEGER primary key autoincrement,
                                 tipo TEXT(50) not null,
                                 categoria TEXT(50) not null
-                                )"""
+                                );"""
     
     sql_create_password = """CREATE TABLE IF NOT EXISTS password(
                                 password 
-                                )"""
+                                );"""
     
     sql_create_orcamento = """CREATE TABLE IF NOT EXISTS orcamento(
                                 id_orcamento INTEGER PRIMARY KEY autoincrement,
                                 id_categoria INTEGER NOT NULL,
                                 categoria TEXT(50) NOT NULL,
                                 limite REAL NOT NULL
-                                )"""
+                                );"""
     
     sql_create_movimentacao = """CREATE TABLE IF NOT EXISTS movimentacao(
                                 id_movimentacao INTEGER PRIMARY KEY autoincrement,
                                 data_movimentacao DATE NOT NULL,
-                                conta_debito TEXT NOT NULL
+                                conta_debito TEXT NOT NULL,
                                 conta_credito TEXT NOT NULL,
                                 valor REAL NOT NULL
-                                )"""
+                                );"""
     
-
-    # Cria a conexão com o banco de dados
-    conn = create_connection(database)
-
-    # Cria as tabelas
-    if conn is not None:
-        
-        # cria a tabela transacoes
-        create_table(conn, sql_create_transacoes_table)
-
-        # cria a tabela conta
-        create_table(conn, sql_create_conta_table)
-        
-        # cria a tabela categoria
-        create_table(conn, sql_create_categoria)
-        
-        # cria a tabela password
-        create_table(conn, sql_create_password)
-        
-        # cria a tabela orcamento
-        create_table(conn, sql_create_orcamento)
-
-        # cria a tabela movimentação
-        create_table(sql_create_movimentacao)
-        
-    else:
-        print("Error! cannot create the database connection.")
-
+    # Lista de comandos SQL para criar todas as tabelas
+    scripts = [sql_create_transacoes_table, sql_create_conta_table, sql_create_categoria, sql_create_password, sql_create_orcamento, sql_create_movimentacao]
+    # Gera a conexão com o banco de dados e cria as tabelas
+    with db.generate_conn() as conn:
+        for script in scripts:
+            db.create_table(conn, script)
 
 if __name__ == "__main__":
     main()
